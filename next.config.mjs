bb9b01bc4f0f2c47ac0b9d513d81e4aa87495c3a
Mukul Meena <mukul.meena@quantinent.in>
2024-06-28 22:42:10 +0530

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    MONGO_USER: "mukul",
+    MONGO_PASS: "mukul_05",
+    DB_URI:
+      "mongodb+srv://mukul:mukul_05@cluster0.gy4l6pq.mongodb.net/Buyitnow?retryWrites=true&w=majority&appName=Cluster0",
+  },
+};
 
 export default nextConfig;
