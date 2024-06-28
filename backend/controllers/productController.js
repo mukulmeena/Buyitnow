fc4b8182fbd31960a52055fc66a6421a67439b72
Mukul Meena <mukul.meena@quantinent.in>
2024-06-28 22:44:43 +0530

    routes for fetching and creating products

---
+++ backend/controllers/productController.js
@@ -0,0 +1,13 @@
+import product from "../models/product";
+
+export const newProduct = async (req, res, next) => {
+  const new_product = await product.create(req.body);
+  res.status(200).json({
+    new_product,
+  });
+};
+
+export const getProducts = async (req, res, next) => {
+  const all_products = await product.find({});
+  res.status(200).json(all_products);
+};
